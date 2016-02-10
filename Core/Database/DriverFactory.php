73c604166435e99acb877ce12a429ed4a7b58818
stardisblue <stardisblue@users.noreply.github.com>
2016-02-10 20:35:10 +0100

    adding PostgreSQL support

--- Core/Database/DriverFactory.php
+++ Core/Database/DriverFactory.php
@@ -6,12 +6,13 @@ use Rave\Core\Database\Driver\GenericDriver;
 use Rave\Core\Exception\UnknownDriverException;
 use Rave\Core\Database\Driver\MySQLDriverPDO\MySQLDriverPDO;
 use Rave\Core\Database\Driver\SQLiteDriverPDO\SQLiteDriverPDO;
+use Rave\Core\Database\Driver\SQLiteDriverPDO\PostgreSQLDriverPDO;
 
 class DriverFactory
 {
     const MYSQL_PDO = 'MySQLPDO';
     const SQLITE_PDO = 'SQLitePDO';
-    const PGSQL = 'PostgreSQLPDO';
+    const POSTGRESQL_PDO = 'PostgreSQLPDO';
 
     public static function get(string $driverConstant): GenericDriver
     {
@@ -20,6 +21,8 @@ class DriverFactory
                 return new MySQLDriverPDO();
             case self::SQLITE_PDO:
                 return new SQLiteDriverPDO();
+            case self::POSTGRESQL_PDO:
+                return new PostgreSQLDriverPDO();
             default:
                 throw new UnknownDriverException('Driver ' . $driverConstant . ' does not exists');
         }
